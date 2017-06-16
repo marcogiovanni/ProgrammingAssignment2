4cf64565de061a4d7808d532178223b56d1c8d03
marcogiovanni <marcogiovanni.crotti@mail.polimi.it>
2017-06-16 19:27:29 +0200

    Update cachematrix.R
    additional comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,11 @@
 ## inverting the matrix in a intelligent way. 
 
-## creating a special matrix. Actually, a list.
+## creating a special matrix. Actually, a list. 
+
+
+## this function takes as argument a matrix and as output it returns a list 
+## which is then used to calculate the inverse of the matrix in an efficient way, so that it is calculate only once (the first time).
+
 
 makeCacheMatrix <- function(x = matrix()) {
   inverse <- NULL 
@@ -19,7 +24,9 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 
-## function calculating the inverse if not done yet.
+## The aim of this function is to calculate the inverse of a matrix.
+## In order to be efficient, it first checks if the calculation of the same matrix has been already done previously. 
+## if it was already done, then it gets it from a list, otherwise it actually calculate the inverse.
 
 cacheSolve <- function(x) {
         ## Return a matrix that is the inverse of 'x'
